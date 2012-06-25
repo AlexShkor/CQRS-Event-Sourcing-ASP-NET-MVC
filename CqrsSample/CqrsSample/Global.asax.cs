e2609889f3f163096082adf3b3749cc129ed0a53
Alex Shkor <alex.shkor@paralect.com>
2012-06-25 19:29:31 +0300

    route fixed

--- CqrsSample/CqrsSample/Global.asax.cs
+++ CqrsSample/CqrsSample/Global.asax.cs
@@ -33,7 +33,7 @@ namespace CqrsSample
             routes.MapRoute(
                 "Default", // Route name
                 "{controller}/{action}/{id}", // URL with parameters
-                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+                new { controller = "Users", action = "Index", id = UrlParameter.Optional } // Parameter defaults
             );
 
         }
